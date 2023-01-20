384e5f3522c2cded68e159745d62e4e630736f84
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-01-20 20:54:22 +0400

    zd

--- lesson3/index.js
+++ lesson3/index.js
@@ -13,17 +13,20 @@ const countries = [
 const ul = document.getElementById('countries');
 const btn = document.getElementById('btn');
 const inpt = document.getElementById('inpt');
-var id = 3
+var id = countries.length + 1;
 
 
 btn.addEventListener('click',()=>{
-    removeAllCountries();
-    countries.push({
-        id: id,
-        country:inpt.value
-    });
-    id++;
-    renderCountries();
+    let inptStr = inpt.value;
+
+    if(isValid(inptStr)){
+        countries.push({
+            id: id,
+            country:inpt.value
+        });
+        id++;
+        renderCountries();
+    }
     inpt.value = '';
 })
 
@@ -35,9 +38,8 @@ function addCountry(obj){
     let but = document.createElement('button');
     but.addEventListener('click',()=>{
 
-        let index = countries.indexOf(countries.find(c => c.id == obj.id))
+        let index = countries.indexOf(obj)
         countries.splice(index,1)        
-        removeAllCountries();
         renderCountries();
 
     })
@@ -53,7 +55,20 @@ function removeAllCountries(){
 }
 
 function renderCountries(){
+    removeAllCountries();
     countries.forEach(c => addCountry(c));
+}
+
+function isValid(str){
+    if(str.length < 1){
+        alert('Input field must not be blank!');
+        return false;
+    }
+    else if(countries.some(c => c.country === str)){
+        alert("Country "+str+" already exists!");
+        return false;
+    }
+    return true;
 }
 
 renderCountries();
